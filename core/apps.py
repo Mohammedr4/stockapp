4f5b8493a99ae247eba87db6f9a3cad49d80a20b
Your Name <your-email@example.com>
2025-09-26 17:01:26 +0100

    Remove emergency superuser creation script

--- core/apps.py
+++ core/apps.py
@@ -4,22 +4,3 @@ class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
 
-    def ready(self):
-        # This is the emergency script. It will run once when the server starts.
-        from django.contrib.auth import get_user_model
-        
-        User = get_user_model()
-        username = 'admin'
-        password = 'stocksavvyadminpassword123'
-        email = 'admin@stocksavvy.com'
-
-        if not User.objects.filter(username=username).exists():
-            print(f'Creating emergency superuser: {username}')
-            User.objects.create_superuser(username, email, password)
-        else:
-            print(f'Emergency superuser {username} already exists. Ensuring password is correct.')
-            user = User.objects.get(username=username)
-            user.set_password(password)
-            user.is_staff = True
-            user.is_superuser = True
-            user.save()
